543a391f087b86f8d4cf200af28760a5af845e85
nicoleoii <N01646918@humber.ca>
2024-09-09 09:16:43 -0400

    Lab1

---
+++ src/Lab1.java
@@ -0,0 +1,26 @@
+public class Lab1{
+    public static void main(String[]args) {
+
+    int int1 = 16;
+    int int2 = 3;
+    int intresult;
+    double int3 = 16;
+    double int4 = 3;
+    double doubleresult;
+
+    System.out.println(int1 + int2);
+    System.out.println(int1 - int2);
+    System.out.println(int1 * int2);
+    System.out.println(int1 / int2);
+    System.out.println(int1 % int2);
+    System.out.println(int1++);
+
+    System.out.println(int3 / int4);
+    System.out.println(Math.round(int3 / int4));
+    System.out.println(Math.pow(int3,3));
+    System.out.println(Math.sqrt(int4));
+
+
+
+    }
+}
